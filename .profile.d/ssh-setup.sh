6053f65937df6ab34e2ffc26136e1a3f11659d85
Ilya Danilkin <nezorflame@gmail.com>
2017-01-25 17:01:23 +0300

    update again

--- .profile.d/ssh-setup.sh
+++ .profile.d/ssh-setup.sh
@@ -13,12 +13,11 @@ chmod 644 ${HOME}/.ssh/heroku_id_rsa.pub
 echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/.ssh/heroku_id_rsa
 chmod 600 ${HOME}/.ssh/heroku_id_rsa
 
-# Start the SSH agent and add host
-eval `ssh-agent -s`
-ssh-add ~/.ssh/heroku_id_rsa
-
 # Preload the known_hosts file
 echo '|1|nGa/epzYJei5Vohi6/wWqNsiobU=|Tih5nQX6aftXmv7wtVcBHpzK2ZQ= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJhx3I5S1scVvmtB9hkfv+tTdT759fUI899fvdjyF7gGq1Bb2xZ2K72gay/iS+a6zUoFw2GYp1dsSqooDhrWTA4=' > ${HOME}/.ssh/known_hosts
+
+# Start the SSH agent and add host
+eval `ssh-agent -s`
 
 # Copy config from the server
 scp ${SSH_USER}@${SSH_ADDRESS}:~/config.toml .
