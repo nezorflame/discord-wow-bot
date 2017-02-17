f1ef07afc2737db2a5a33fbe12f2680a25fb13b5
Ilya Danilkin <nezorflame@gmail.com>
2017-02-17 19:00:32 +0300

    add some old logic

--- bot.go
+++ bot.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strings"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/golang/glog"
 )
@@ -53,4 +56,79 @@ func (b *Bot) messageCreate(s *discordgo.Session, mes *discordgo.MessageCreate) 
 	if mes.Author.ID == b.ID {
 		return
 	}
+}
+
+/* Tries to call a method and checking if the method returned an error, if it
+did check to see if it's HTTP 502 from the Discord API and retry for
+`attempts` number of times. */
+func retryOnBadGateway(f func() error) {
+	var err error
+	for i := 0; i < 3; i++ {
+		if err = f(); err != nil {
+			if strings.HasPrefix(err.Error(), "HTTP 502") {
+				// If the error is Bad Gateway, try again after 1 sec.
+				time.Sleep(1 * time.Second)
+				continue
+			} else {
+				// Otherwise panic !
+				glog.Fatal(err)
+			}
+		} else {
+			// In case of no error, return.
+			return
+		}
+	}
+}
+
+func sendMessage(session *discordgo.Session, chID string, message string) (err error) {
+	glog.Info("SENDING MESSAGE:", message)
+	retryOnBadGateway(func() error {
+		return sendFormattedMessage(session, chID, message)
+	})
+	return
+}
+
+func sendFormattedMessage(session *discordgo.Session, chID string, message string) (err error) {
+	if i := len(message); len(message) > 1999 {
+		for i > 1999 {
+			messageSlice := strings.Split(message, "\n")
+			mes := messageSlice[0]
+			l := len(messageSlice)
+			if l == 2 {
+				_, err = session.ChannelMessageSend(chID, mes+"\n")
+				if err != nil {
+					return err
+				}
+				_, err = session.ChannelMessageSend(chID, messageSlice[1])
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+		Loop:
+			for j := 1; j < l-1; j++ {
+				mes += "\n" + messageSlice[j]
+				if len(mes+"\n"+messageSlice[j+1]) > 1999 {
+					if strings.HasPrefix(mes, "```") {
+						_, err = session.ChannelMessageSend(chID, mes+"```")
+					} else {
+						_, err = session.ChannelMessageSend(chID, mes)
+					}
+					if err != nil {
+						return err
+					}
+					message = strings.Replace(message, mes, "", 1)
+					if strings.HasPrefix(mes, "```") {
+						message = "```" + message
+					}
+					break Loop
+				}
+			}
+			i = len(message)
+		}
+		_, err = session.ChannelMessageSend(chID, message)
+	} else {
+		_, err = session.ChannelMessageSend(chID, message)
+	}
+	return
 }
