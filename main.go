f38a08f55d5997473262dbed95978353c592fb06
Ilya Danilkin <i.danilkin@maximatelecom.ru>
2017-02-17 18:50:02 +0300

    fix warnings

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import "github.com/golang/glog"
+import "flag"
 
 // WoWBot is a Discord WoW guild bot
 var WoWBot *Bot
 
 func main() {
+	flag.Parse()
 	glog.CopyStandardLogTo("INFO")
 	glog.Info("Loading config...")
 	LoadConfig()
