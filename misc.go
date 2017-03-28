842c60e2d0eb200e02fd58d1a295a0b7daef117d
Ilya Danilkin <i.danilkin@maximatelecom.ru>
2017-03-28 18:15:13 +0300

    some moving

--- misc.go
+++ misc.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"os/exec"
 	"strings"
 	"time"
 	"unicode"
 )
+
+// ExecuteCommand - function which runs the shell command
+func ExecuteCommand(command, dir string, args []string) (output string, err error) {
+	var bOutput []byte
+	cmd := exec.Command(command, args...)
+	cmd.Dir = dir
+	bOutput, err = cmd.Output()
+	output = string(bOutput)
+
+	return
+}
 
 func splitStringByCase(splitString string) (result string) {
 	l := 0
