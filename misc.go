88b55b08095bb43f1f54ddef2671385576d9bf0b
Ilya Danilkin <i.danilkin@maximatelecom.ru>
2017-03-28 18:16:57 +0300

    more moving

--- misc.go
+++ misc.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"os/exec"
 	"strings"
 	"time"
@@ -14,7 +15,35 @@ func ExecuteCommand(command, dir string, args []string) (output string, err erro
 	cmd.Dir = dir
 	bOutput, err = cmd.Output()
 	output = string(bOutput)
+	return
+}
 
+// Unmarshal makes the GuildInfo from the []byte
+func (gi *GuildInfo) Unmarshal(body []byte) error {
+	return json.Unmarshal(body, gi)
+}
+
+// Unmarshal makes the Realms from the []byte
+func (r *Realms) Unmarshal(body []byte) error {
+	return json.Unmarshal(body, r)
+}
+
+// Unmarshal makes the Character from the []byte
+func (c *Character) Unmarshal(body []byte) error {
+	return json.Unmarshal(body, c)
+}
+
+// Unmarshal makes the Item from the []byte
+func (i *Item) Unmarshal(body []byte) error {
+	return json.Unmarshal(body, i)
+}
+
+// GetURLFromJSON returns short goo.gl link
+func GetURLFromJSON(body []byte) (apiResponseID string, err error) {
+	apiResponse := new(URLShortenerAPIResponse)
+	if err = json.Unmarshal(body, apiResponse); err == nil {
+		apiResponseID = apiResponse.ID
+	}
 	return
 }
 
